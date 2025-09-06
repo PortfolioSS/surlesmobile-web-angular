f75436e57f60aec56a1b3147c041261d11b21fb2
Steve Surles <SteveSurles@gmail.com>
2025-09-06 10:28:19 -0400

    Configure environment with Cognito settings for local development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  production: true,
-  apiBase: "https://<api-host>",
+  production: false,
+  apiBase: "http://localhost:5000",
   auth: {
     region: "us-east-1",
-    userPoolId: "<POOL_ID>",
-    userPoolWebClientId: "<CLIENT_ID>",
+    userPoolId: "us-east-1_9ZpfRImkY",
+    userPoolWebClientId: "2o43ov6eflc2bmv6k6tpb9dnk0",
     oauth: {
-      domain: "<DOMAIN>.auth.us-east-1.amazoncognito.com",
+      domain: "surlesmobile-auth.auth.us-east-1.amazoncognito.com",
       scope: ["openid","email","portfolio/read","portfolio/write","portfolio/admin"],
       redirectSignIn: "http://localhost:4200/callback",
       redirectSignOut: "http://localhost:4200",
